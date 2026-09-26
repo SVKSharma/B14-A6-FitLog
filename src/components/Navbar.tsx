396d4964afd63d1f260bfce73755cc288974eb97
svksharma <301975153+SVKSharma@users.noreply.github.com>
2026-09-26 09:35:10 +0600

    changed logo text color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,8 +26,8 @@ export const Navbar = () => {
         {/* Logo */}
         <Link href="/" className="flex items-center gap-2">
           <FaDumbbell className="text-[#a6e22e] text-2xl -rotate-45" />
-          <span className="font-oswald font-extrabold text-xl tracking-wider uppercase">
-            FITLOG
+          <span className="font-oswald font-extrabold text-xl tracking-wider uppercase text-[#a6e22e]">
+            <span className="text-white">FIT</span>LOG
           </span>
         </Link>
 
